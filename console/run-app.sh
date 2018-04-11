659c1d0fa0b8f190c21910125a47833a802c06b6
JULANDW <juergen.landwehr@daimler.com>
2018-04-11 16:27:55 +0200

    Adapt Mac run-app.sh to start app from current working directory

--- console/run-app.sh
+++ console/run-app.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-java -jar build/libs/scoreboard-console.jar
+
+FILENAME=scoreboard-console.jar
+LIBS_PATH=build/libs
+CWD=$(dirname "$0")
+
+if [ -f "$CWD/$FILENAME" ]
+then
+  JAR_FILE=$CWD/$FILENAME
+else
+  JAR_FILE=$LIBS_PATH/$FILENAME
+fi
+
+java -jar $JAR_FILE
