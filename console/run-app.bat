8d5002ba3bee4dc24c8e6aac9b7f7be52c748c67
JULANDW <juergen.landwehr@daimler.com>
2018-04-11 16:07:11 +0200

    Adapt windows run-app.bat to start application from libs- and current path, added build-app.bat

--- console/run-app.bat
+++ console/run-app.bat
@@ -1,3 +1,11 @@
+SET FILENAME=scoreboard-console.jar
+SET LIBS_PATH=build/libs/
+SET CWD=%~p0
 
+IF EXIST "%CWD%%FILENAME%" (
+  SET JAR_FILE=%CWD%%FILENAME%
+) ELSE (
+  SET JAR_FILE=%LIBS_PATH%%FILENAME%
+)
 
-start java -jar build/libs/scoreboard-console.jar
+start java -jar %JAR_FILE%
