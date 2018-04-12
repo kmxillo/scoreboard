cb9a37a2fa076faefd3b8d1e864830bd7e43236e
ALBRAN1 <alexander.bran@daimler.com>
2018-04-12 15:18:39 +0200

    implemented Integration test

--- integration-tests/src/test/java/net/johanneslink/scoreboard/it/IntegrationTest.java
+++ integration-tests/src/test/java/net/johanneslink/scoreboard/it/IntegrationTest.java
@@ -2,8 +2,8 @@ package net.johanneslink.scoreboard.it;
 
 import java.io.*;
 import java.util.*;
+import java.util.List;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import net.johanneslink.scoreboard.console.Main;
 import org.junit.jupiter.api.*;
 
@@ -14,17 +14,23 @@ class IntegrationTest {
 	private ByteArrayOutputStream stdout;
 	private PrintStream originalStdout;
 
+	private ByteArrayInputStream stdin;
+	private InputStream originalStdin;
+
 	@BeforeEach
 	void init() {
+		originalStdin = System.in;
 		stdout = new ByteArrayOutputStream();
 		PrintStream ps = new PrintStream(stdout);
 		originalStdout = System.out;
 		System.setOut(ps);
+
 	}
 
 	@AfterEach
 	void reset() {
 		System.setOut(originalStdout);
+		System.setIn(originalStdin);
 	}
 
 	@Test
@@ -32,14 +38,28 @@ class IntegrationTest {
 		startConsoleApp();
 		List<String> stdoutLines = stdoutLines();
 		assertEquals(1, stdoutLines.size());
-		assertEquals( "000:000", stdoutLines.get(0));
+		assertEquals("000:000", stdoutLines.get(0));
 	}
 
 	@Test
-	@Disabled
-	void consoleAppInterpretsInputLineByLineAndCanBeQuit() {
-//		 Todo: Redirect stdin to accept input from tests
-		fail("Not implemented yet");
+	void consoleAppInterpretsInputLineByLineAndCanBeQuit() throws InterruptedException, IOException {
+		String inputText = String.format("A%n" + "1%n" + "B%n" + "1%n" + "2%n" + "3%n" + "4%n" + "Q%n");
+		stdin = new ByteArrayInputStream(inputText.getBytes());
+		System.setIn(stdin);
+
+		startConsoleApp();
+
+		List<String> stdoutLines = stdoutLines();
+		Assertions.assertAll(//
+				() -> assertEquals(8, stdoutLines.size()), //
+				() -> assertEquals("Team A selected", stdoutLines.get(1)), //
+				() -> assertEquals("001:000", stdoutLines.get(2)), //
+				() -> assertEquals("Team B selected", stdoutLines.get(3)), //
+				() -> assertEquals("001:001", stdoutLines.get(4)), //
+				() -> assertEquals("001:003", stdoutLines.get(5)), //
+				() -> assertEquals("001:006", stdoutLines.get(6)), //
+				() -> assertEquals("Unknown command '4'", stdoutLines.get(7)) //
+		);
 	}
 
 	private void startConsoleApp(final String... args) throws InterruptedException {
