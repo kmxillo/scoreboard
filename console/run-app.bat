7b2a3cec465dabeb0518156ebfbaa9a6e9f17138
ALBRAN1 <alexander.bran@daimler.com>
2018-04-12 16:23:53 +0200

    updated bat script

--- console/run-app.bat
+++ console/run-app.bat
@@ -3,9 +3,9 @@ SET LIBS_PATH=build/libs/
 SET CWD=%~p0
 
 IF EXIST "%CWD%%FILENAME%" (
-  SET JAR_FILE=%CWD%%FILENAME%
+  SET JAR_FILE="%CWD%%FILENAME%"
 ) ELSE (
-  SET JAR_FILE=%LIBS_PATH%%FILENAME%
+  SET JAR_FILE="%LIBS_PATH%%FILENAME%"
 )
 
-start java -jar "%JAR_FILE%"
+start java -jar %JAR_FILE%
